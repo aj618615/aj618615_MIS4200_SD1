4f1d6fe5c3a290eee78f7979dfed1c0822cec2ae
Analise Johnson <aj618615@ohio.edu>
2019-09-11 11:39:49 -0400

    initial model work

---
+++ Models/order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace aj618615_MIS4200_SD1.Models
+{
+    public class order
+    {
+        public int orderID { get; set; }
+        public int customerID { get; set; }
+        public virtual customer customer { get; set; }
+        public DateTime orderDate { get; set; }
+
+
+    }
+}
